726ef26b3be60a26134937dfdb7262c0ba9c9e7a
yllan vinicius ferreira gurgel <yllan.gurgel@sms.intranet>
2020-10-15 11:24:50 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root>
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas Tarefas';
+
+  /**
+   *
+   */
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao supermercado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+  }
+
+  remove(todo: Todo) {
+    const index = this.todos.indexOf(todo);
+    if (index !== -1) {
+      this.todos.splice(index, 1);
+    }
+  }
+
+  markAsDone() {}
+
+  markAsUndone() {}
 }
---
+++ src/models/todo.model.ts
@@ -0,0 +1,6 @@
+export class Todo {
+  /**
+   *
+   */
+  constructor(public id: Number, public title: String, public done: Boolean) {}
+}
